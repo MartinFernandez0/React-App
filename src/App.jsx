26eb20fa1bfded4ddb366ed5fe93c9de343628aa
MartinFernandez0 <martinexefe@gmail.com>
2024-08-01 21:27:23 -0300

    actualizacion app react

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
-import MyButton from './MyButton';
-import MyImage from './image';
+import Container from './components/Container';
+import Titulo from './components/Titulo';
+import Formulario from './components/Formulario';
 
 function App() {
-  const onClickHundler = () => {
-    alert('Click!');
-  };
 
-  const name = "Acept";
-  const title = "My Button";
-  const parrafo = "Tarea de clases";
-  const imageUrl = "https://i.pinimg.com/564x/7f/fd/2f/7ffd2fd91dab1a292cd3aada059beaac.jpg";
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(event);
+  }
 
   return (
-
-    <div>
-      <MyButton
-        name={name}
-        title={title}
-        parrafo={parrafo}
-        onClick={onClickHundler}
-      />
-
-      <MyImage img={imageUrl} />
-
-    </div>
-  );
+    <>
+    <Container>
+      <Titulo value="ToDo - App" />
+      <Formulario onSubmitHandler={onSubmitHandler}/>
+    </Container>
+    </>
+  ); 
 }
-
 export default App;
